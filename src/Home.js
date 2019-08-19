3e304d05908f17a959ea1ced62319f9da3ee019d
cabrallrs@gmail.com <cabrallrs@gmail.com>
2019-08-19 18:36:56 -0300

    navbar with bootstrap

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+import { functionTypeAnnotation } from '@babel/types';
+import NavBar from'./ui/NavBar';
+
+const Home = () => {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  };
+  
+  export default Home;
---
+++ src/ui/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './NavBar.css';
+import logo from './assets/logo.svg';
+
+
+const NavBar = () => {
+    return (    
+            <nav className="navbar navbar-expand-lg navbar-light bg-ligth scrolling-navbar fixed-top">
+                <div className='container'>   
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} width="150" height="40"  className="d-inline-block align-top" alt="economizze.me"/>
+                    </a>
+    
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Planos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Ajuda</a>
+                            </li>
+                        </ul>
+                        
+                        <form class="form-inline my-2 my-lg-0">
+                            <button type="button" class="btn btn-outline-success my-2 my-sm-0 "href="#">Comece agora</button> 
+                        </form>
+
+                    </div>
+                </div>
+            </nav> 
+          
+
+
+
+    );
+};
+
+export default NavBar;

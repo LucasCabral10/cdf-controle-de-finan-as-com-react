9327113b5d5dd6f3826643c588909323f309a3a1
cabrallrs@gmail.com <cabrallrs@gmail.com>
2019-08-19 22:16:58 -0300

    navbar ok

--- src/ui/NavBar.js
+++ src/ui/NavBar.js
@@ -8,14 +8,14 @@ const NavBar = () => {
             <nav className="navbar navbar-expand-lg navbar-light bg-ligth scrolling-navbar fixed-top">
                 <div className='container'>   
                     <a className="navbar-brand" href="#">
-                        <img src={logo} width="150" height="40"  className="d-inline-block align-top" alt="economizze.me"/>
+                        <img src={logo} width="170" height="40"  className="d-inline-block align-top" alt="economizze.me"/>
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="NavbarN" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                        <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
